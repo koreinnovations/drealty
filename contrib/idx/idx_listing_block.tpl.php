8b5f02e5815ddb84c3267df38b28b57f2715cbc6
chris lewis <camidoo@172849.no-reply.drupal.org>
2010-06-10 21:03:44 +0000

    Initial Import v2

---
+++ contrib/idx/idx_listing_block.tpl.php
@@ -0,0 +1,10 @@
+<?php
+$path = drupal_get_path('theme', 'century21');
+?>
+
+
+<div class="listing-sidebar-action-buttons">
+    <div><a href="#" title="Request Information"><img class="rollover" src="/<?php print $path; ?>/images/request-info-normal.png" alt="Request Information" /></a></div>
+    <div><a href="#" title="Request Information"><img class="rollover" src="/<?php print $path; ?>/images/schedule-showing-normal.png" alt="Request Information" /></a></div>
+    <div><a href="#" title="Request Information"><img class="rollover" src="/<?php print $path; ?>/images/save-property-normal.png" alt="Request Information" /></a></div>
+</div>

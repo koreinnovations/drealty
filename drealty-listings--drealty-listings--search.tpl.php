0b9a1b85aa779b2476464cb59527c4dccf58c598
camidoo <chris@camidoo.com>
2011-09-17 16:11:53 -0500

    sdf

--- drealty-listings--drealty-listings--search.tpl.php
+++
@@ -1,5 +0,0 @@
-<div>
-  <div>
-    <?php print theme_image_style(array('style_name' => 'listing_search_thumb', 'path' => $content['mls_images']['0']['filepath'], 'alt' => $content['listing_id'])); ?>
-  </div>
-</div>
